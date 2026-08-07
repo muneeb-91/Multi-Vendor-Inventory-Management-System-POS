d4f60f97e404a1283cbd40a85fb7508d24470a48
Muneeb Ur Rehman <muneeburrehman538@gmail.com>
2026-08-07 23:05:54 +0500

    supplier controller file formatted

--- backend/controllers/supplier.controller.js
+++ backend/controllers/supplier.controller.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 import Supplier from "../models/supplier.model.js";
-import Product from '../models/product.model.js';
+import Product from "../models/product.model.js";
 
 export const addSupplier = async (req, res) => {
   const { supplierName, phone, email, address } = req.body;
+
   const existingSupplier = await Supplier.findOne({
     vendorId: req.user.vendorId,
     supplierName: {
