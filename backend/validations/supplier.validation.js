c70eda324067894a0a18263a226b08dd84c1bce4
Muneeb Ur Rehman <muneeburrehman538@gmail.com>
2026-08-14 20:37:47 +0500

    supplier backend validation changes

--- backend/validations/supplier.validation.js
+++ backend/validations/supplier.validation.js
@@ -5,4 +5,5 @@ export const addSupplierSchema = joi.object({
   phone: joi.string().trim().required(),
   email: joi.string().email().trim().optional(),
   address: joi.string().trim(),
+  status: joi.string().trim().valid("active", "inactive"),
 });
